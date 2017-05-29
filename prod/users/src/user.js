fa04adf4a9a001a94ba3e5d092abd9c0540ade75
Martin Porter <martinjporter@gmail.com>
2017-05-29 22:37:27 +0200

    Added subsdocument for post

--- prod/users/src/user.js
+++ prod/users/src/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -11,7 +11,8 @@ const UserSchema = new Schema({
 		},
 		required: [true, 'Name is required.']
 	},
-	postCount: Number
+	postCount: Number,
+	posts: [PostSchema]
 });
 
 const User = mongoose.model('user',UserSchema)
