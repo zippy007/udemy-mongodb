20afb6107091fbaabd2ecd69a263ec239ac06735
Martin Porter <martinjporter@gmail.com>
2017-05-29 20:30:10 +0200

    Validate function

--- prod/users/test/validation_test.js
+++ prod/users/test/validation_test.js
@@ -3,10 +3,18 @@ const User = require('../src/user')
 
 describe('Validating Records', () => {
 
-  it('requires a username', () => {
+  it('requires a username', (done) => {
     const user = new User({ name: undefined });
     const validationResult = user.validateSync();
-    const { message } = validationResult.errors.name
+    const { message } = validationResult.errors.name;
     assert(message === 'Name is required.');
+    done();
+  });
+
+  it('requires a user name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === 'Name must be longer than 2 characters.');
   });
 });
