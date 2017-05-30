2e753d9013f6d74d797e7cffbad33848510e7bf2
Martin Porter <martinjporter@gmail.com>
2017-05-30 16:17:50 +0200

    Full association fetching

--- prod/users/test/association_test.js
+++ prod/users/test/association_test.js
@@ -33,4 +33,26 @@ describe('Associations', (done) => {
       });
   });
 
+  it('Saves a full relation tree (asssociations)', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+        done();
+      });
+  });
+
 });
