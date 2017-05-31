fc76e140e98d61f7f0b7ae714ea2f280ae98c1dd
Martin Porter <martinjporter@gmail.com>
2017-05-31 17:03:44 +0200

    Search Working

--- prod/UpStarMusic/database/queries/SearchArtists.js
+++ prod/UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,41 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
+  console.log(criteria);
+
+  const query = Artist.find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 })   // ES6 Syntax
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([ query, Artist.count() ])
+      .then((results) => {
+          return {
+            all: results[0],
+            count: results[1],
+            offset: offset,
+            limit: limit
+          };
+      });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+  return query;
+
 };
